9ada93fd6c712cc0cee5d2f978eac7420644aa51
Le xuan loc <lexuanloc2512@gmail.com>
2019-06-15 08:12:58 +0700

    first commit

---
+++ session2/hinhtron.py
@@ -0,0 +1,4 @@
+bankinh = int(input("nhap ban kinh vao day "))
+pi = 3.14
+s = bankinh **2 * pi  
+print("dien tich la ", s ,)
---
+++ session2/tamgiac.py
@@ -0,0 +1,8 @@
+chieucao = int(input("nhap chieu cao vao day "))
+canhday = int(input("nhap canh day vao day "))
+s = chieucao * canhday * 0.5
+print("dientichla: ", s ,)
+
+
+
+
